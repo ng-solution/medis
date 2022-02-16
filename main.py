be22214d8f6efe7890cb1b58e80553c1a9707fd4
ng-solution <89936592+ng-solution@users.noreply.github.com>
2022-02-17 00:57:53 +0200

    oko

--- main.py
+++ main.py
@@ -3,8 +3,14 @@
 # coding: utf-8
 from __future__ import division, print_function
 
+
+def suma(a, b):
+    return a+b
+
+
 def main():
     print("Hello World!", 5/2 == float(5)/2)
+
 
 if __name__ == "__main__":
     main()
